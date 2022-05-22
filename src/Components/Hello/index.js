ff04561241df10243ac18b93e931c034f9f83f1b
StepchUk <stepchyk@gmail.com>
2022-05-22 19:16:55 +0300

    Added styles

--- src/Components/Hello/index.js
+++ src/Components/Hello/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hello() {
   return (
-    React.createElement('h1', {}, 'Hello World')
+    React.createElement('h1', {style:{color:"#F65241"}}, 'Hello World')
   );
 }
 
