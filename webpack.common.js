c2cc55fece6aebf436902456f12440fd98655efa
StepchUk <stepchyk@gmail.com>
2022-05-31 23:54:27 +0300

    Switch to new webpack features

--- webpack.common.js
+++ webpack.common.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'production',
-  devtool: false,
-  entry: ['./src/index.js'],
+  entry: {
+    app: './src/index.js'
+  },
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
@@ -15,16 +15,6 @@ module.exports = {
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules'],
     extensions: ['.js', '.jsx', '.json']
-  },
-  devServer: {
-    static: '.build',
-    compress: true,
-    port: 3001,
-    open: true
-  },
-  performance: {
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
   },
   optimization: {
     splitChunks: {
---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer : {
+    static: '.build',
+    compress: true,
+    port: 3000
+    //open: true
+  }
+})
