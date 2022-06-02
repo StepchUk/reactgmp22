b537f4c762e288fa722fa61296303270ba363cb2
StepchUk <stepchyk@gmail.com>
2022-06-02 09:21:04 +0300

    Removed test component. Added Assets and Mock data

---
+++ src/API/PostService.js
@@ -0,0 +1,62 @@
+import pulp from '../Assets/Images/puop_fiction.jpg'
+import bohemrhaps from '../Assets/Images/bohemian_rhapsody.jpg'
+import killb from '../Assets/Images/kill_bill_2.jpg'
+import incep from '../Assets/Images/inception.jpg'
+import reserdogs from '../Assets/Images/reservoir_dogs.jpg'
+
+export const videos = [
+  {
+    id: 1,
+    title: 'Pulp Fiction',
+    posterPath: pulp,
+    year: 2004,
+    genre: ['Action & Adventure']
+  },
+  {
+    id: 2,
+    title: 'Bohemian Rhapsody',
+    posterPath: bohemrhaps,
+    year: 2003,
+    genre: [
+      'Drama',
+      'Biography',
+      'Music'
+    ]
+  },
+  {
+    id: 3,
+    title: 'Kill Bill: Vol 2',
+    posterPath: killb,
+    year: 1994,
+    genre: [
+      'Oscar winning Movie'
+    ]
+  },
+  {
+    id: 4,
+    title: 'Avengers: War of Infinity',
+    posterPath: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    year: 2004,
+    genre: [
+      'Action & Adventure'
+    ]
+  },
+  {
+    id: 5,
+    title: 'Inception',
+    posterPath: incep,
+    year: 2003,
+    genre: [
+      'Action & Adventure'
+    ]
+  },
+  {
+    id: 6,
+    title: 'Reservoir dogs',
+    posterPath: reserdogs,
+    year: 1994,
+    genre: [
+      'Oscar winning Movie'
+    ]
+  }
+]
