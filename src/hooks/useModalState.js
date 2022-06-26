29c7c5664f24e98915213cda4eae7ec0d6372482
StepchUk <stepchyk@gmail.com>
2022-06-26 21:02:15 +0300

    Added useModalState hook

---
+++ src/hooks/useModalState.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const ModalTypes = {
+  ADD: 'add',
+  EDIT: 'edit',
+  CONFIRM: 'confirm'
+};
+
+const INIT_STATE = { type: null, data: null };
+
+const useModalState = () => {
+  const [modal, setModal] = useState(INIT_STATE);
+
+  const isAddType = modal.type === ModalTypes.ADD;
+  const isEditType = modal.type === ModalTypes.EDIT;
+  const isConfirmType = modal.type === ModalTypes.CONFIRM;
+
+  const close = () => setModal(INIT_STATE);
+  const showAdd = () => setModal({ type: ModalTypes.ADD });
+  const showEdit = (data) => setModal({ type: ModalTypes.EDIT, data });
+  const showConfirm = (data) => setModal({ type: ModalTypes.CONFIRM, data });
+
+  const data = modal.data;
+
+  return {
+    data,
+    isAddType,
+    isEditType,
+    isConfirmType,
+    showAdd,
+    showEdit,
+    showConfirm,
+    close
+  };
+};
+
+export default useModalState;
