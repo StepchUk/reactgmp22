893ce28010f5ad5cd12882baf2db5d73de996540
StepchUk <stepchyk@gmail.com>
2022-06-30 00:34:08 +0300

    Added eslint to the project

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,11 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
   output: {
     filename: '[name].bundle.js',
@@ -14,40 +15,41 @@ module.exports = {
   },
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules'],
-    extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json'],
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
-    }
+    },
   },
   module: {
     rules: [
       {
         test: /\.js|\.jsx$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
-          'css-loader'
-        ]
+          'css-loader',
+        ],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         type: 'asset/resource',
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
+    new EslintWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[name].css'
-    })
-  ]
-}
+      chunkFilename: '[name].css',
+    }),
+  ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,10 +8,10 @@ module.exports = merge(common, {
     static: '.build',
     compress: true,
     port: 3001,
-    //open: true
+    // open: true
   },
   performance: {
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
-})
+    maxAssetSize: 512000,
+  },
+});
