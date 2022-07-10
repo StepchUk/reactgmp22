6158ac21ec62ebbcaa3ee19665e26a7eed4c7c33
StepchUk <stepchyk@gmail.com>
2022-07-10 20:36:54 +0300

    Form recreated with Formik

--- src/Components/UI/videoformmodal/VideoFormModal.jsx
+++ src/Components/UI/videoformmodal/VideoFormModal.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
 import MyButton from '../button/MyButton';
-import Input from '../input/Input';
-import style from './VideoFormModal.module.css';
+// import Input from '../input/Input';
+// import style from './VideoFormModal.module.css';
 
 const emptyVideo = {
   title: '',
@@ -11,7 +17,27 @@ const emptyVideo = {
   genre: ['horro', 'drama'],
   rating: '',
   runtime: '',
-  description: '',
+  overview: '',
+};
+
+const validation = (values) => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Required';
+  }
+  if (!values.posterPath) {
+    errors.posterPath = 'Required';
+  }
+  if (!values.runtime) {
+    errors.runtime = 'Required';
+  }
+  if (!values.overview) {
+    errors.overview = 'Required';
+  }
+  if (!values.genre) {
+    errors.genre = 'Required';
+  }
+  return errors;
 };
 
 function VideoForm({ editVideo, onSubmit, hideModal }) {
@@ -23,11 +49,9 @@ function VideoForm({ editVideo, onSubmit, hideModal }) {
     }
   }, [editVideo]);
 
-  const addNewVideo = (e) => {
-    e.preventDefault();
-
+  const onSubmitFormic = (values, { setSubmitting }) => {
     const newVideo = {
-      ...video,
+      ...values,
     };
 
     onSubmit(newVideo);
@@ -35,81 +59,88 @@ function VideoForm({ editVideo, onSubmit, hideModal }) {
     setVideo(emptyVideo);
 
     hideModal();
+    setSubmitting(true);
   };
-
-  const reset = (e) => {
-    e.preventDefault();
-
-    setVideo(emptyVideo);
-  };
-
-  const handleVideoChange = (key, value) => setVideo({ ...video, [key]: value });
 
   return (
-    <form>
-      <Input
-        id="name"
-        value={video.title}
-        lable="title"
-        placeholder="Enter movie title"
-        onChange={(value) => handleVideoChange('title', value)}
-      />
-      <br />
-      <Input
-        id="posterpath"
-        value={video.posterPath}
-        lable="movie url"
-        placeholder="https://"
-        onChange={(value) => handleVideoChange('posterPath', value)}
-      />
-      <br />
-      <Input
-        id="genre"
-        value={video.genre}
-        lable="genre"
-        placeholder="Select Genre"
-        onChange={(value) => handleVideoChange('genre', value)}
-      />
-      <br />
-      <Input
-        id="year"
-        value={video.year}
-        lable="release date"
-        placeholder="Select Date"
-        onChange={(value) => handleVideoChange('year', value)}
-      />
-      <br />
-      <Input
-        id="rating"
-        value={video.rating}
-        lable="rating"
-        placeholder="7.8"
-        onChange={(value) => handleVideoChange('rating', value)}
-      />
-      <br />
-      <Input
-        id="runtime"
-        value={video.runtime}
-        lable="runtime"
-        placeholder="minutes"
-        onChange={(value) => handleVideoChange('runtime', value)}
-      />
-      <br />
-      <label htmlFor="overview">overview</label>
-      <textarea id="overview" value={video.description} onChange={(e) => setVideo({ ...video, description: e.target.value })} placeholder="Movie description" />
-      <br />
-      <MyButton onClick={reset} className="button__gray-blurred">
-        reset
-      </MyButton>
-      <MyButton onClick={addNewVideo} className="button__red">
-        submit
-      </MyButton>
-    </form>
-
+    <Formik
+      initialValues={video}
+      validate={validation}
+      onSubmit={onSubmitFormic}
+      enableReinitialize
+    >
+      {({ isSubmitting, resetForm }) => (
+        <Form>
+          <ErrorMessage name="title" component="div" />
+          <Field
+            type="text"
+            name="title"
+            placeholder="Enter movie title"
+          />
+          <br />
+          <ErrorMessage name="releaseDate" component="div" />
+          <Field
+            type="text"
+            name="releaseDate"
+            placeholder="Select Date"
+          />
+          <br />
+          <ErrorMessage name="posterPath" component="div" />
+          <Field
+            type="text"
+            name="posterPath"
+            placeholder="https://"
+          />
+          <br />
+          <ErrorMessage name="rating" component="div" />
+          <Field
+            type="text"
+            name="rating"
+            placeholder="7.8"
+          />
+          <br />
+          <ErrorMessage name="genre" component="div" />
+          <Field
+            type="text"
+            name="genre"
+            placeholder="Select Genre"
+          />
+          <br />
+          <ErrorMessage name="runtime" component="div" />
+          <Field
+            type="text"
+            name="runtime"
+            placeholder="minutes"
+          />
+          <br />
+          <ErrorMessage name="overview" component="div" />
+          <Field
+            type="text"
+            as="textarea"
+            name="overview"
+            placeholder="Movie description"
+          />
+          <br />
+          <MyButton onClick={resetForm} className="button__gray-blurred">
+            reset
+          </MyButton>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
+VideoForm.defaultProps = {
+  editVideo: undefined,
+  onSubmit: null,
+  hideModal: null,
+};
+
 VideoForm.propTypes = {
+  editVideo: PropTypes.oneOf([PropTypes.object]),
   onSubmit: PropTypes.func,
   hideModal: PropTypes.func,
 };
