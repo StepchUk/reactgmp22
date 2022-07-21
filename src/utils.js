923314321b2a32740aadce1c141c2b78e6afe414
StepchUk <stepchyk@gmail.com>
2022-07-22 00:21:42 +0300

    Moved utils to main level

--- src/utils.js
+++ src/utils.js
@@ -17,7 +17,47 @@ const trasnform = (o, fn) => Object.fromEntries(
   Object.entries(o).map(([k, v]) => [fn(k), v]),
 );
 
+const isIsoDate = (str) => !/\d{4}-\d{2}-\d{2}/.test(str);
+
+const isValidUrl = (url) => {
+  try {
+    return new URL(url);
+  } catch (e) {
+    return false;
+  }
+};
+
 export const toCamelCase = (arrayOfObjects) => arrayOfObjects.map((o) => trasnform(o, camelFn));
 export const toSnakeCase = (o) => trasnform(o, snakeFn);
 
 export const getYear = (date) => date.split('-')[0];
+
+export const movieFormValidation = (values) => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Required';
+  }
+
+  if (!isIsoDate(values.releaseDate)) {
+    errors.releaseDate = 'Incorrect date format. Date must be in YYYY-mm-dd format';
+  }
+
+  if (!values.posterPath) {
+    errors.posterPath = 'Required';
+  } else if (!isValidUrl(values.posterPath)) {
+    errors.posterPath = 'URL is not correct';
+  }
+
+  if (!values.runtime) {
+    errors.runtime = 'Required';
+  }
+
+  if (!values.overview) {
+    errors.overview = 'Required';
+  }
+
+  if (!values.genres) {
+    errors.genres = 'Required';
+  }
+  return errors;
+};
